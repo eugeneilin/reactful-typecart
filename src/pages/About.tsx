9bc68fd87955f721ed659fad7bcc96abffecf371
Eugene Ilin <eugenepianist@gmail.com>
2023-04-18 20:14:53 -0400

    v1.2

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,7 +4,7 @@ export function About() {
   return (
     <Container>
       <h1 className='cursive mt-5'>Chef's Pens Collection</h1>
-      <h5 className='mb-4 primary-color'>App Version 1.1</h5>
+      <h5 className='mb-4 primary-color'>App Version 1.2</h5>
       <p className='mb-5'>
         These pens are custom made with the best chefs in mind. All proceeds go to feeding hungry
         people. Thank you for your support!
